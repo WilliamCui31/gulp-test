56a016ef067299316c7567a2ae97267378bd393f
WilliamCui31 <444738577@qq.com>
2016-12-09 20:32:22 +0800

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp'),
+    less = require('gulp-less'),
+    autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('css',function(){
+  gulp.src('src/less/**/*.less')
+      .pipe(less())
+      .pipe(autoprefixer({ browsers: ['last 2 versions', 'Android >= 4.0'] }))
+      .pipe(gulp.dest('dest/css/'));
+});
+
+gulp.task('default',['css']);
